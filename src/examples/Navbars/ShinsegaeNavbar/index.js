d166815b075310b69a376ebf942680e3b86421f4
bururaru <bururaru@gmail.com>
2022-09-22 13:19:23 +0900

    add: navbar 추가

---
+++ src/examples/Navbars/ShinsegaeNavbar/index.js
@@ -0,0 +1,28 @@
+import { color } from "@mui/system";
+import React from "react";
+import Icon from "@mui/material/Icon";
+import logo from "/Users/chang/develop/react/ReactProject-NEWDINING/src/assets/images/icons/flags/logo.png";
+
+function index(props) {
+    const box = {
+        backgroundColor: "#D70C19",
+        height: "50px",
+        color: "white",
+        padding: "10px",
+        display: "flex",
+        justifyContent: "center",
+    };
+    const logo_style = {
+        height: "30px",
+    };
+    // const logo = require("react/ReactProject-NEWDINING/src/assets/images/icons/flags/logo.png");
+    // const logo = require("/Users/chang/develop/react/ReactProject-NEWDINING/src/assets/images/icons/flags/logo.png");
+    return (
+        <div style={box}>
+            <div>신세계 아이앤씨</div>
+            <img src={logo} alt="logo" style={logo_style}></img>
+        </div>
+    );
+}
+
+export default index;
